935e82a612b69f58eb92509632811925a7d6eb85
TheoLugagne <lugagne.theo@gmail.com>
2026-05-20 14:45:57 +0200

    installation et setup Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-white font-sans text-dark">
+      <p className="p-8 font-serif text-4xl text-primary">
+        Theo Lugagne — Portfolio
+      </p>
     </div>
   );
 }
