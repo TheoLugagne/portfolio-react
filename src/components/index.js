bf2c71311409e0425c166a56f773237ece8a0891
TheoLugagne <lugagne.theo@gmail.com>
2026-06-06 18:36:51 +0200

    [INIT] TS conversion : remove js

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Hero from './Hero/Hero';
-import Projects from './Projects/Projects';
-import Button from './ui/Button';
-import Container from './layout/Container';
-import SectionHeading from './layout/SectionHeading';
-import Navbar from './Navbar/Navbar';
-
-export { Hero, Projects, Button, Container, SectionHeading, Navbar };
